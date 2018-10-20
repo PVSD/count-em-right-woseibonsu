903732bec266598a18ee60b985cae561ff5fa61f
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-10-20 17:54:44 -0400

    Hi Mr. P

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String input, sp[];
+        while (true) {
+            System.out.print("Please type a sentence and press Enter.\nPress Exit to leave.\n");
+            input = sc.nextLine();
+            if(input.equalsIgnoreCase("EXIT")) {
+              break;
+            }
+            input = input + "Z";
+            input = input.toUpperCase();
+
+            sp = input.split("(S\\s*A)");
+
+            System.out.println("There are " + (sp.length - 1) + " occurrences.\n");
+
+
+        }
     }
 }
